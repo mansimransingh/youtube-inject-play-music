48b4d9f4cce476e948772256dcc4a8646e30089f
Mansimran Singh <me@mansimransingh.com>
2015-02-01 16:35:40 +0000

    is a music video based on youtube category

--- content.js
+++ content.js
@@ -1,3 +1,25 @@
-console.log('------------------------');
-console.log('INJECT TEST');
-console.log('------------------------');
+//check if video category contains music
+var is_music = false;
+var description_extras_list = document.getElementById('watch-description-extras').getElementsByTagName("li");
+
+for(var i=0; i < description_extras_list.length; i++){
+    if(description_extras_list[i].getElementsByTagName('h4').length > 0 
+        && description_extras_list[i].getElementsByTagName('h4')[0].innerHTML.toLowerCase().indexOf('category') > -1){
+            //we got category 
+            var list_of_categories = description_extras_list[i]
+                .getElementsByClassName('watch-info-tag-list')[0].getElementsByTagName('li');
+
+                for(var j=0;j<list_of_categories.length;j++){
+                    if(list_of_categories[i].innerHTML.toLowerCase().indexOf('music') > -1){
+                        //categories contains music
+                        is_music = true;
+                        break;
+                    }
+                }
+            //this was the categories list
+            break;
+    }
+}
+
+            console.log('---------------');
+            console.log("IS MUSIC "+is_music)
